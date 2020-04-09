f72bb29f5937401abc2f35811ee4f2fbd9d2722f
Sam Deane <sam@elegantchaos.com>
2020-04-09 23:06:35 +0100

    Initial Commit

---
+++ Maqemu/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Maqemu
+//
+//  Created by Sam Deane on 09/04/2020.
+//  Copyright © 2020 Elegant Chaos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
