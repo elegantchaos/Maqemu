ab37b5fdf79f03d4fe0b0461ff4db1d10102a02e
Sam Deane <sam@elegantchaos.com>
2020-04-10 12:20:20 +0100

    Added ReleaseTools. Improved icons. Improved Project Layout. Added Configs.

---
+++ Sources/Maqemu/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Maqemu
+//
+//  Created by Sam Deane on 09/04/2020.
+//  Copyright © 2020 Elegant Chaos. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Sources/Maqemu/Resources/BridgingHeader.h
@@ -0,0 +1,10 @@
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+//  Created by Sam Deane on 10/04/2020.
+//  All code (c) 2020 - present day, Elegant Chaos Limited.
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+#ifndef Maqemu_Bridging_Header_h
+#define Maqemu_Bridging_Header_h
+
+
+#endif /* Maqemu_Bridging_Header_h */
