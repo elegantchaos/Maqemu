ca02ffad9922ddad0d4205949232d8cf53c65d81
Sam Deane <sam@elegantchaos.com>
2020-04-14 11:43:32 +0100

    Inherit from BasicApplication

--- Sources/Maqemu/AppDelegate.swift
+++ Sources/Maqemu/AppDelegate.swift
@@ -1,26 +1,12 @@
-//
-//  AppDelegate.swift
-//  Maqemu
-//
-//  Created by Sam Deane on 09/04/2020.
-//  Copyright © 2020 Elegant Chaos. All rights reserved.
-//
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+//  Created by Sam Deane on 10/04/2020.
+//  All code (c) 2020 - present day, Elegant Chaos Limited.
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 import Cocoa
+import ApplicationExtensions
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
-    }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-    }
-
-
+class AppDelegate: BasicApplication {
 }
 
